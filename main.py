92c86882fc9edd320a7e74f8894c0e55dd39f105
Abel Gómez Méndez <abelmetaltele@gmail.com>
2024-05-13 06:32:43 -0400

    otro endpoint

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ Crea un endpoint para recuperar una cita por su ID.
 Crea un endpoint para actualizar una cita existente.
 Crea un endpoint para eliminar una cita por su ID.
     """
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from typing import List
 
@@ -27,15 +27,23 @@ class Cita(BaseModel):
 class ListaDeCitas(BaseModel):
     lista: List[Cita]
 
-cita1 = Cita(ID=1, Fecha="2024", Titulo="Gandalf" ,Descripcion="Todo lo que tienes que hacer es que hacer con el tiempo que se te ha dado")
+cita1 = Cita(ID=1, Fecha="2024", Titulo="Gandalf" ,Descripcion="Todo lo que debes es decidir que hacer con el tiempo que se te ha dado")
 lista_de_citas = []
 
-@app.post('/crear_cita')
+@app.post('/crear_cita') # crea nueva cita y la agrega a la lista
 async def crear_cita(cita:Cita) -> List[Cita]:
     lista_de_citas.append(cita)
     return lista_de_citas
 print (lista_de_citas)
+
 @app.get('/')
 async def observar_lista() -> dict:
-    return {"lista": lista_de_citas} 
+    return {"lista": lista_de_citas}
+
+@app.get('/cita_por_id {id_cita}')
+async def devolver_cita_segun_id(id_cita:int) -> Cita:
+    for cita in lista_de_citas:
+        if cita.ID == id_cita:
+            return cita
+    raise HTTPException(status_code=404, detail="No se ha encontrado ninguna cita con el ID proporcionado")
 
